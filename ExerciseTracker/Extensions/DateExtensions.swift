a0ebfe7c208e025625ba5062f52f850eb5049819
Mark Stewart <mas8771@me.com>
2025-10-03 20:01:19 -0400

    Cosmetic changes

--- ExerciseTracker/Extensions/DateExtensions.swift
+++ ExerciseTracker/Extensions/DateExtensions.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 extension Date {
-    /// Returns the date at the very beginning of the current day (12:00:00 AM).
+        /// Returns the date at the very beginning of the current day (12:00:00 AM).
     var startOfDay: Date {
         return Calendar.current.startOfDay(for: self)
     }
 
-    /// Returns the date at the very end of the current day (11:59:59 PM).
+        /// Returns the date at the very end of the current day (11:59:59 PM).
     var endOfDay: Date {
         let calendar = Calendar.current
         
-        // Find the start of the next day
+            // Find the start of the next day
         let startOfNextDay = calendar.date(byAdding: .day, value: 1, to: self.startOfDay)!
         
-        // Subtract one second/millisecond to get the end of the current day
+            // Subtract one second/millisecond to get the end of the current day
         return calendar.date(byAdding: .second, value: -1, to: startOfNextDay)!
     }
 }
